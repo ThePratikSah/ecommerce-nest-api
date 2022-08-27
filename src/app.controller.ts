a277e69c6eae634d2b03fdbf149a99f33d3c3c30
Pratik Sah <shivam3448@gmail.com>
2022-08-27 23:21:51 +0530

    Added User module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { Users } from '@prisma/client';
+import { v4 as uuid } from 'uuid';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +8,24 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAllUsers(): Promise<Users[]> {
+    return this.appService.getAllUsers();
+  }
+
+  @Post()
+  addNewUser() {
+    const user: Users = {
+      userId: uuid(),
+      email: 'pratiksah@hotmail.com',
+      firstName: 'Pratik',
+      lastName: 'Sah',
+      phone: '8709105800',
+      emailVerified: false,
+      phoneVerified: false,
+      password: '',
+      isDeleted: false,
+    };
+
+    return this.appService.addNewUser(user);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Users } from '@prisma/client';
+import { DatabaseService } from './database/database.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private databaseService: DatabaseService) {}
+  getAllUsers(): Promise<Users[]> {
+    return this.databaseService.users.findMany();
+  }
+
+  addNewUser(user: Users) {
+    const { userId, email, phone, password, lastName, firstName } = user;
+    return this.databaseService.users.create({
+      data: {
+        userId,
+        email,
+        phone,
+        lastName,
+        firstName,
+        password,
+      },
+    });
   }
 }
