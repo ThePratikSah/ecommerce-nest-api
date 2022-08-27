eccec7cec8850e4bf05202ae700f7491970070a0
Pratik Sah <shivam3448@gmail.com>
2022-08-28 02:03:09 +0530

    Added controller and service for categories

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/database/database.module';
+import { CategoryController } from './category.controller';
+import { CategoryService } from './category.service';
 
-@Module({})
+@Module({
+  imports: [DatabaseModule],
+  providers: [CategoryService],
+  controllers: [CategoryController],
+})
 export class CategoryModule {}
